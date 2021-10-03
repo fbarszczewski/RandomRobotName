96b744f188ba659bd95411ad3078124c64303ba8
filip barszczewski <fbarszczewski@outlook.com>
2021-10-03 13:39:54 +0200

    Dodaj pliki projektów.

---
+++ RobotName.cs
@@ -0,0 +1,15 @@
+using System;
+public class Robot
+{
+    public string Name { get; set; }
+
+    public Robot()
+    {
+        Name = "x";
+    }
+
+    public void Reset()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
